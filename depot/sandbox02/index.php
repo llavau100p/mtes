1fc8c9c0fc54b4ac1158d40869942c35646677e1
Laurent Lavaud <laurent.lavaud@developpement-durable.gouv.fr>
2022-11-20 20:17:33 +0100

    sandbox02

---
+++ depot/sandbox02/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP - sandbox02</title>
+</head>
+<body style="background-color:<?php echo $_SERVER['EDCS_ENV_BGCOLOR'];?>;color:<?php echo $_SERVER['EDCS_ENV_FONTCOLOR'];?>;" >
+
+  <h1>Sandbox02</h1>
+
+<?php
+
+  // Les variables
+  $servername = $_SERVER['EDCS_ENV_DB_HOSTNAME'];
+  $username = $_SERVER['EDCS_ENV_DB_USERNAME'];
+  $password = $_SERVER['EDCS_ENV_DB_PASSWORD'];
+  $dbname = $_SERVER['EDCS_ENV_DB_NAME'];
+
+  // La connexion
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  // La requête
+  $sql = "SELECT name, file FROM characters";
+  $result = $conn->query($sql);
+
+  // Le parcours des résultats
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      echo "name: " . $row["name"] . "<br>\n";
+    }
+  } else {
+    echo "0 results";
+  }
+
+  // La fermeture de la connexion
+  $conn->close();
+?>
+
+</body>
+</html>

93f86657144c0f485f41fc07d9b6999062d50eb6
Laurent Lavaud <laurent.lavaud@developpement-durable.gouv.fr>
2022-11-24 09:43:18 +0100

    ajout envoi d'un mail de test

--- depot/sandbox02/index.php
+++ depot/sandbox02/index.php
@@ -1,44 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP - sandbox02</title>
-</head>
-<body style="background-color:<?php echo $_SERVER['EDCS_ENV_BGCOLOR'];?>;color:<?php echo $_SERVER['EDCS_ENV_FONTCOLOR'];?>;" >
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - sandbox02</title>
+  </head>
+  <body style="background-color:<?php echo $_SERVER['EDCS_ENV_BGCOLOR'];?>;color:<?php echo $_SERVER['EDCS_ENV_FONTCOLOR'];?>;" >
 
-  <h1>Sandbox02</h1>
+    <h1>Sandbox02</h1>
 
-<?php
+    <?php
 
-  // Les variables
-  $servername = $_SERVER['EDCS_ENV_DB_HOSTNAME'];
-  $username = $_SERVER['EDCS_ENV_DB_USERNAME'];
-  $password = $_SERVER['EDCS_ENV_DB_PASSWORD'];
-  $dbname = $_SERVER['EDCS_ENV_DB_NAME'];
+     // Les variables
+     $servername = $_SERVER['EDCS_ENV_DB_HOSTNAME'];
+     $username = $_SERVER['EDCS_ENV_DB_USERNAME'];
+     $password = $_SERVER['EDCS_ENV_DB_PASSWORD'];
+     $dbname = $_SERVER['EDCS_ENV_DB_NAME'];
 
-  // La connexion
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
+     // La connexion
+     $conn = new mysqli($servername, $username, $password, $dbname);
+     if ($conn->connect_error) {
+       die("Connection failed: " . $conn->connect_error);
+     }
 
-  // La requête
-  $sql = "SELECT name, file FROM characters";
-  $result = $conn->query($sql);
+      // La requête
+      $sql = "SELECT name, file FROM characters";
+      $result = $conn->query($sql);
 
-  // Le parcours des résultats
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      echo "name: " . $row["name"] . "<br>\n";
-    }
-  } else {
-    echo "0 results";
-  }
+      // Le parcours des résultats
+      if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+          echo "name: " . $row["name"] . "<br>\n";
+        }
+      } else {
+        echo "0 results";
+      }
 
-  // La fermeture de la connexion
-  $conn->close();
-?>
+      // La fermeture de la connexion
+      $conn->close();
+    ?>
 
-</body>
+    <br>
+
+    <?php
+                                                    
+     $to = "laurent.lavaud@developpement-durable.gouv.fr";     
+     $subject = "Test mail";
+     $message = "Coucou de l interieur de l instance";
+     $from = "laurent.lavaud@developpement-durable.gouv.fr";
+     $headers = "From:" . $from;
+
+     echo "Envoi d'un petit mail à " . $to;
+     
+     mail($to,$subject,$message,$headers)
+   ?>
+
+  </body>
 </html>
